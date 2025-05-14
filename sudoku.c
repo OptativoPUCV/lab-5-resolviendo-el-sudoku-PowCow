18826421e9b910aaba907a53b85c32dcb663d3e7
Your Name <you@example.com>
2025-05-14 09:29:25 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -64,7 +64,7 @@ int is_valid(Node* n){ //hacer
     }
   }
 
-  for(int k = 0; k < 9; k++){
+  for(int k = 0; k < 9; k++){ //recorrer matriz 3x3
     int box[10] = {0};
     for(int p = 0; p < 9; p++){
       int i = 3*(k/3) + (p/3);
@@ -100,9 +100,14 @@ List* get_adj_nodes(Node* n){ //LISTO (mirar si cambiar)
   return list;
 }
 
-
 int is_final(Node* n){ //hacer
-    return 0;
+  for (int a = 0 ; a < 9 ; a++){
+    for (int b = 0 ; b < 9 ; b++){
+      int numerito = n->sudo[a][b] ;
+      if (numerito == 0) return 0 ;
+    }
+  }
+  return 1;
 }
 
 Node* DFS(Node* initial, int* cont){ //hacer
